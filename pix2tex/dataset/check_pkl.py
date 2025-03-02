1f9fd6c48283f9cd82ee3007b8910b5515361e07
Yuxiang <2101776247@qq.com>
2025-03-02 13:52:51 +0800

    check the dataset info

---
+++ pix2tex/dataset/check_pkl.py
@@ -0,0 +1,21 @@
+import pickle
+from pix2tex.dataset.dataset import Im2LatexDataset
+from os.path import join
+
+# path to pkl
+pkl_file_path = "test.pkl"
+
+# load pkl
+with open(pkl_file_path, 'rb') as f:
+    dataset = pickle.load(f)
+
+#iter(dataset)
+
+# print info.
+print("Data structure:")
+for dimensions, pairs in list(dataset.data.items())[:100]:  # total images
+    print(f"Images with dimensions {dimensions}:")
+    for i, (latex, image_path) in enumerate(pairs[:1]):  # one by one
+        print(f"  Item {i+1}:")
+        print(f"    LaTeX Code: {latex}")
+        print(f"    Image Path: {image_path}")
