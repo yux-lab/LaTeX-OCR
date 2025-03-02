dc2f6bf19385e00dd5ab2e50d93f58fc52176cc8
Yuxiang <2101776247@qq.com>
2025-03-02 18:58:46 +0800

    added counter

--- pix2tex/dataset/show_pkl.py
+++ pix2tex/dataset/show_pkl.py
@@ -9,13 +9,17 @@ pkl_file_path = "data/test.pkl"
 with open(pkl_file_path, 'rb') as f:
     dataset = pickle.load(f)
 
+count = 0
 #iter(dataset)
 
 # print info.
 print("Data structure:")
-for dimensions, pairs in list(dataset.data.items())[:100]:  # total images
+for dimensions, pairs in list(dataset.data.items())[:40000]:  # total images
     print(f"Images with dimensions {dimensions}:")
     for i, (latex, image_path) in enumerate(pairs[:1]):  # one by one
+        count = count+1
         print(f"  Item {i+1}:")
         print(f"    LaTeX Code: {latex}")
         print(f"    Image Path: {image_path}")
+
+print(count)
