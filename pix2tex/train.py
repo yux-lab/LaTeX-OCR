be5dc9e6a7df8dbad9debf4a6a603e32a9951eed
Yuxiang <2101776247@qq.com>
2025-03-10 10:24:58 +0800

    # only save when higher score and named after performance

--- pix2tex/train.py
+++ pix2tex/train.py
@@ -16,8 +16,10 @@ from pix2tex.utils import in_model_path, parse_args, seed_everything, get_optimi
 
 
 def train(args):
+    # load data
     dataloader = Im2LatexDataset().load(args.data)
     dataloader.update(**args, test=False)
+    # load valdata
     valdataloader = Im2LatexDataset().load(args.valdata)
     valargs = args.copy()
     valargs.update(batchsize=args.testbatchsize, keep_smaller_batches=True, test=True)
@@ -33,17 +35,37 @@ def train(args):
     if args.load_chkpt is not None:
         model.load_state_dict(torch.load(args.load_chkpt, map_location=device))
 
-    def save_models(e, step=0):
-        torch.save(model.state_dict(), os.path.join(out_path, '%s_e%02d_step%02d.pth' % (args.name, e+1, step)))
+    def save_models(e, step=0, bleu=0, edit_dist=0, token_acc=0):
+        bleu_str = f"{bleu:.3f}"
+        ed_str = f"{edit_dist:.3f}"
+        acc_str = f"{token_acc:.3f}"
+
+        save_path = os.path.join(out_path,
+                                 f"{args.name}_e{e + 1:02d}_step{step}_BLEU:{bleu_str}_ED:{ed_str}_ACC:{acc_str}.pth")
+
+        torch.save({
+            'epoch': e + 1,
+            'step': step,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': opt.state_dict(),
+            'scheduler_state_dict': scheduler.state_dict(),
+            'bleu_score': bleu_score,
+            'edit_distance': edit_distance,
+            'token_accuracy': token_accuracy
+        }, save_path)
+        # torch.save(model.state_dict(), os.path.join(out_path, '%s_e%02d_step%02d.pth' % (args.name, e+1, step)))
         yaml.dump(dict(args), open(os.path.join(out_path, 'config.yaml'), 'w+'))
 
+    # optimizer default: Adam
     opt = get_optimizer(args.optimizer)(model.parameters(), args.lr, betas=args.betas)
+    # scheduler default: StepLR
     scheduler = get_scheduler(args.scheduler)(opt, step_size=args.lr_step, gamma=args.gamma)
 
     microbatch = args.get('micro_batchsize', -1)
     if microbatch == -1:
         microbatch = args.batchsize
 
+    # loop
     try:
         for e in range(args.epoch, args.epochs):
             args.epoch = e
@@ -54,22 +76,35 @@ def train(args):
                     total_loss = 0
                     for j in range(0, len(im), microbatch):
                         tgt_seq, tgt_mask = seq['input_ids'][j:j+microbatch].to(device), seq['attention_mask'][j:j+microbatch].bool().to(device)
+                        # calculate forward
                         loss = model.data_parallel(im[j:j+microbatch].to(device), device_ids=args.gpu_devices, tgt_seq=tgt_seq, mask=tgt_mask)*microbatch/args.batchsize
+
+                        # calculate backward
                         loss.backward()  # data parallism loss is a vector
                         total_loss += loss.item()
+
+                        # gradient clipping
                         torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
+                    # update optimizer
                     opt.step()
+
+                    # update scheduler
                     scheduler.step()
                     dset.set_description('Loss: %.4f' % total_loss)
                     if args.wandb:
                         wandb.log({'train/loss': total_loss})
                 if (i+1+len(dataloader)*e) % args.sample_freq == 0:
+                    # performance
                     bleu_score, edit_distance, token_accuracy = evaluate(model, valdataloader, args, num_batches=int(args.valbatches*e/args.epochs), name='val')
                     if bleu_score > max_bleu and token_accuracy > max_token_acc:
                         max_bleu, max_token_acc = bleu_score, token_accuracy
-                        save_models(e, step=i)
+                        # only save when higher score
+                        save_models(e, step=i, bleu=bleu_score, edit_dist=edit_distance, token_acc=token_accuracy)
+                        #save_models(e, step=i)
             if (e+1) % args.save_freq == 0:
-                save_models(e, step=len(dataloader))
+                save_models(e, step=len(dataloader), bleu=max_bleu, edit_dist=edit_distance, token_acc=max_token_acc)
+                # only save when higher score
+                #save_models(e, step=len(dataloader))
             if args.wandb:
                 wandb.log({'train/epoch': e+1})
     except KeyboardInterrupt:
@@ -97,6 +132,7 @@ if __name__ == '__main__':
     if args.wandb:
         if not parsed_args.resume:
             args.id = wandb.util.generate_id()
+        # mode="disabled"
         wandb.init(config=dict(args), resume='allow', name=args.name, id=args.id)
         args = Munch(wandb.config)
     train(args)
